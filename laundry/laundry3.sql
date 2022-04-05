9347fc58b6783d1e160d534759458ca2be70c46b
ⓔ <60589563+ekaputridr@users.noreply.github.com>
2022-04-05 22:11:43 +0700

    Add files via upload

---
+++ laundry/laundry3.sql
@@ -0,0 +1,216 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 05, 2022 at 05:11 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laundry3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `jenis_kelamin` enum('pria','wanita') NOT NULL,
+  `tlp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id_member`, `nama`, `alamat`, `jenis_kelamin`, `tlp`) VALUES
+(3, 'jay', 'jl. bandung 5', 'pria', '08239713139'),
+(6, 'hujan', 'bekasi', 'wanita', '08111112398');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outlet`
+--
+
+CREATE TABLE `outlet` (
+  `id_outlet` int(11) NOT NULL,
+  `nama_outlet` varchar(100) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `alamat` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `outlet`
+--
+
+INSERT INTO `outlet` (`id_outlet`, `nama_outlet`, `id_user`, `alamat`) VALUES
+(2, 'outlet dua', 6, 'jakut'),
+(3, 'outlet lu', 15, 'jakut');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paket`
+--
+
+CREATE TABLE `paket` (
+  `id_paket` int(11) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `paket`
+--
+
+INSERT INTO `paket` (`id_paket`, `jenis`, `harga`) VALUES
+(1, 'kaos', 9000),
+(2, 'selimut', 5000),
+(3, 'sweater', 11500),
+(4, 'karpet', 25000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `id_paket` int(11) NOT NULL,
+  `berat` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `tgl_bayar` date NOT NULL,
+  `status_proses` varchar(20) NOT NULL,
+  `status_bayar` varchar(20) NOT NULL,
+  `metode` varchar(100) NOT NULL,
+  `id_outlet` int(11) NOT NULL,
+  `total` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_member`, `id_paket`, `berat`, `tgl`, `tgl_bayar`, `status_proses`, `status_bayar`, `metode`, `id_outlet`, `total`) VALUES
+(23, 3, 1, 2, '2022-03-25', '2022-03-25', 'selesai', 'bayar', 'tunai', 2, 9),
+(26, 3, 1, 2, '2022-03-25', '2022-03-29', 'baru', 'bayar', 'debit', 2, 18),
+(30, 6, 2, 6, '2022-03-26', '2022-03-27', 'baru', 'belum_bayar', '', 3, 0),
+(31, 3, 1, 3, '2022-03-30', '2022-03-31', 'baru', 'belum_bayar', 'tunai', 2, 27);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`, `role`) VALUES
+(4, 'mark', 'mark@gmail.com', 'admin', 'admin', 'admin'),
+(6, 'riki', 'riki@gmail.com', 'owner', 'owner', 'owner'),
+(9, 'omil', 'omil@gmail.com', 'kasir', 'kasir', 'kasir'),
+(14, 'moreaw', 'morwawaw@gmail.com', 'kasir2', 'kasir2', 'kasir'),
+(15, 'moreaw', 'morwawaw@gmail.com', 'owner2', 'owner2', 'owner');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `outlet`
+--
+ALTER TABLE `outlet`
+  ADD PRIMARY KEY (`id_outlet`);
+
+--
+-- Indexes for table `paket`
+--
+ALTER TABLE `paket`
+  ADD PRIMARY KEY (`id_paket`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `outlet`
+--
+ALTER TABLE `outlet`
+  MODIFY `id_outlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `paket`
+--
+ALTER TABLE `paket`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
